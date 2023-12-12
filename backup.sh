610c742b85c8497987cef1eac8574de3ca8b7af2
Rafael Costa <rafael.txct@gmail.com>
2023-12-12 11:11:28 -0300

    first commit/Uploading script

---
+++ backup.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+find /BackupDirectory/* -mmin +354 -exec rm -rf {} \;   # Set timer to delete old files
+
+TIME=`date +%I-%d-%b`                                   # This Command will read the date.
+FILENAME=projects-$TIME.tar.gz                          # The filename including the date.
+SRCDIR=/DirectoryToBackup                               # Source backup folder.
+DESDIR=/SendBackup                                      # Destination of backup file.
+tar -cpzf $DESDIR/$FILENAME $SRCDIR                     # Execute backcup
